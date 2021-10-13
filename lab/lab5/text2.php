3b6af28e41fd28aec49d56162e4ec07fb13d8f05
Nguyễn Hữu Thành Nam <76080072+thanhtuanlk11@users.noreply.github.com>
2021-10-13 08:04:08 +0700

    php

---
+++ lab/lab5/text2.php
@@ -0,0 +1,7 @@
+<?php
+    $url = 'https://www.w3resource.com/php-exercises/php-basic-exercises.php';
+    $url=parse_url($url);
+    echo 'Scheme : '.$url['scheme']."\n";
+    echo 'Host : '.$url['host']."\n";
+    echo 'Path : '.$url['path']."\n";
+?> 
